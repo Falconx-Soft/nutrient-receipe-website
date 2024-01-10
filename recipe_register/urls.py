84190876536750faf953c5bdb496e140062ec728
Hamza-Ijaz12 <hamzah.ijazh@gmail.com>
2024-01-11 01:18:12 +0500

    10-1-24

--- recipe_register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('find',views.recipesearch,name='find'),
-    path('detail/<str:pk>',views.recipedetail,name='detail'),
-    
-    
-]
